34e93ca6bba09a99ffe8c781b3b6c0584695159a
satoshi-sh <satoss1108@gmail.com>
2026-04-04 16:11:54 -0500

    Finished 142 understanding safe areas

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -60,6 +60,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _openExpenseOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) {
